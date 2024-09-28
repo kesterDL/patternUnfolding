bbe1af9d14bdd36d758343d8ddc13ee8712c1b29
kesterDL <kesterdl@gmail.com>
2024-09-27 20:52:17 -0500

    embedded spotify player

--- webUI/src/Components/shared-components/LatestEpisodeTile/HeroTile.jsx
+++ webUI/src/Components/shared-components/LatestEpisodeTile/HeroTile.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 import styles from "./HeroTile.module.css";
 import wreath from "../../../logos/wovenWreath.svg";
 
 function HeroTile() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlayPodcast = () => {
+    setIsPlaying(true); // Show the Spotify player when clicked
+  };
+
   return (
     <>
       <Helmet>
@@ -19,10 +25,29 @@ function HeroTile() {
           <h3 className={styles.episodeTitle}>The Wind Weaves</h3>
           <p className={styles.description}>
             Opening the Eye of the World. Hot spiced cider, honey cakes, and the
-            Black Rider. Walking the well tred path of Tolkien, then going
+            Black Rider. Walking the well-trodden path of Tolkien, then going
             beyond.
           </p>
-          <div className={styles.listenNow}>Listen Now</div>
+
+          {/* Show the Listen Now button or the Spotify player */}
+          {!isPlaying ? (
+            <div className={styles.listenNow} onClick={handlePlayPodcast}>
+              Listen Now
+            </div>
+          ) : (
+            <div className={styles.spotifyPlayer}>
+              <iframe
+                style={{ borderRadius: "12px" }}
+                src="https://open.spotify.com/embed/episode/0J5rVspYAeZlvvzlqussUS?utm_source=generator"
+                width="100%"
+                height="152" // Adjusted height for better layout
+                frameBorder="0"
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+                title="Spotify Episode - The Wind Weaves"
+              ></iframe>
+            </div>
+          )}
         </div>
       </div>
     </>
